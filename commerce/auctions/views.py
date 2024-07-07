d714a62d1a3cc62b13456bc3a56529c39130a387
Archit <sharmaarchit014@gmail.com>
2024-07-07 21:04:32 +0530

    Made listing page

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -21,7 +21,10 @@ class NewListingForm(forms.Form):
     bid = forms.IntegerField(label="Minimum Bid", initial=0)
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listings.objects.filter(status = True)
+    return render(request, "auctions/index.html",{
+        "listings":listings
+    })
 
 
 def login_view(request):
@@ -87,8 +90,111 @@ def new(request):
             category = form.cleaned_data["category"]
             bid = form.cleaned_data["bid"]
 
-            f = Listings(item=name, description=description, start_bid=bid, image=image, status=True, seller=user)
+            f = Listings(category = category, item=name, description=description, start_bid=bid, image=image, status=True, seller=user)
             f.save()
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/new.html",{
+                "form": form
+            })
     return render(request, "auctions/new.html",{
         "form": NewListingForm()
+    })
+
+@login_required
+def item(request, title):
+    listing = Listings.objects.get(item = title)
+    cmnt = Comment.objects.filter(item = listing)
+    user = User.objects.get(username = request.user)
+    winner = listing.current_bid.user
+    lists = user.item.all()
+    is_watchlist = False
+    is_active = listing.status
+    for list in lists:
+        if list.item == title:
+            is_watchlist = True
+            break
+    
+    if request.method == "POST":
+        if listing.seller == request.user:
+            listing.status = False
+            listing.save()
+            return HttpResponseRedirect(reverse("index"))
+        
+        watchlist = request.POST['watchlist']
+        comment = request.POST['comment']
+        bid = request.POST['bid']
+
+        if is_watchlist:
+            if watchlist == "true":
+                if listing.seller != request.user:
+                    for list in lists:
+                        if list.item == title:
+                            user.item.remove(list)
+                else:
+                    message = "You cannot remove watchlist from your own product"
+                    return render(request, "auctions/error.html",{
+                        "message":message
+                    })
+        else:
+            if watchlist == "true":
+                if listing.seller != request.user:
+                    user.item.add(listing)
+                else:
+                    message = "You cannot add watchlist to your own product"
+                    return render(request, "auctions/error.html",{
+                        "message":message
+                    })
+        
+        if comment:
+            if listing.seller != request.user:
+                f = Comment(user = request.user, item = listing, comment = comment)
+                f.save()
+            else:
+                message = "You cannot add comments to your own product"
+                return render(request, "auctions/error.html",{
+                    "message":message
+                })
+        if bid:
+            bid = int(bid)
+            if listing.seller != request.user:
+                if listing.current_bid != None:
+                    if bid > listing.current_bid.bid:
+                        f = Bid(user = request.user, item = listing, bid = bid)
+                        f.save()
+                        listing.current_bid = f
+                        listing.save()
+                    else :
+                        message = "Bid should be higher than current bid"
+                        return render(request, "auctions/error.html",{
+                            "message":message
+                        })
+                else:
+                    if bid > listing.start_bid:
+                        f = Bid(user = request.user, item = listing, bid = bid)
+                        f.save()
+                        listing.current_bid = f
+                        listing.save()
+            else:
+                message = "You cannot bid to your own product"
+                return render(request, "auctions/error.html",{
+                    "message":message
+                })
+        return render(request, "auctions/item.html",{
+            "listing":listing,
+            "user":request.user,
+            "comments":cmnt,
+            "is_watchlist":is_watchlist,
+            "is_active":is_active,
+            "winner":winner
+        })
+
+
+    return render(request, "auctions/item.html",{
+        "listing":listing,
+        "user":request.user,
+        "comments":cmnt,
+        "is_watchlist":is_watchlist,
+        "is_active":is_active,
+        "winner":winner
     })
